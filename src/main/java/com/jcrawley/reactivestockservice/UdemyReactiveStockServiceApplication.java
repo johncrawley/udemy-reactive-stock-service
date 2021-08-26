2d19876a9b8fab31d84b1f800973b85257b5d1a4
johncrawley <johncrawley1@gmail.com>
2021-08-26 12:44:21 +0100

    Initial Commit

---
+++ src/main/java/com/jcrawley/reactivestockservice/UdemyReactiveStockServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jcrawley.reactivestockservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyReactiveStockServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyReactiveStockServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jcrawley/reactivestockservice/UdemyReactiveStockServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcrawley.reactivestockservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyReactiveStockServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
